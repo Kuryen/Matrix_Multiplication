0fe36c073ca117a0bd2ecb58e366cb43c5a1f67a
Mohammad <137657989+TheAnnoyingProgrammer@users.noreply.github.com>
2024-04-23 22:49:09 -0700

    Update groupAssignmentOne.py
    Thred Implementation

--- groupAssignmentOne.py
+++ groupAssignmentOne.py
@@ -13,22 +13,24 @@ def Mat_Mult_S(MAT_A, MAT_B):
 
 def Mat_Mult_P(MAT_A, MAT_B, nPROC):
     MAT_C = np.zeros((A.shape[0], B.shape[1]))
-    pool = mp.Pool(processes=nPROC)
+    Threads = [0 for _ in range(nPROC)]
     for PROC in range(nPROC):
-        pool.apply_async(Sub_Mat_Mult_P, args=(MAT_A, MAT_B, MAT_C, PROC, nPROC))
-    pool.close()
-    pool.join()
+        Threads[PROC] = threading.Thread(target=Sub_Mat_Mult_P, args=(MAT_A, MAT_B, MAT_C, PROC, nPROC))
+    for Thread in Threads:
+        Thread.start()
+    for Thread in Threads:
+        Thread.join()
     return MAT_C
 
 def Sub_Mat_Mult_P(MAT_A, MAT_B, MAT_C, PROC, nPROC):
-    for i in range(A.shape[0]):
+    for i in range(PROC, A.shape[0], nPROC):
         for j in range(B.shape[1]):
-            for k in range(PROC, B.shape[0], nPROC):
+            for k in range(B.shape[0]):
                 MAT_C[i, j] += MAT_A[i, k] * MAT_B[k, j]
 
 if __name__ == '__main__':
 
-    z = 10
+    z = 100
     n = z
     m = z
     l = z
@@ -50,6 +52,3 @@ if __name__ == '__main__':
     t2_f = time.time()
     print(t2_f - t2_o)
     print(D, "\n")
-
-
-
